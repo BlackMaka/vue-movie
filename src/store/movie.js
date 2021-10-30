915386bbd29a2de77608b7d1851d8ec2ae79f450
completedLee <completed0728@gmail.com>
2021-10-30 18:29:09 +0900

    영화검색 구현

--- src/store/movie.js
+++ src/store/movie.js
@@ -4,17 +4,12 @@ export default{
   namespaced: true,
   state: () => ({
     movies : [],
-    message : '',
-    loading : false,
   }),
   getters:{},
   mutations : {
     updateState(state, payload){
       Object.keys(payload).forEach(key =>{
         state[key] = payload[key];
-        // state.movies = payload.movies
-        // state.message = payload.message
-        // state.loading = payload.loading
       })
     },
     resetMovies(state){
@@ -22,15 +17,13 @@ export default{
     }
   },
   actions: { 
-    async searchMovies(context, payload){
+    async searchMovies({commit}, payload){
       const { title, type, number, year } = payload;
       const OMDB_API_KEY = '7035c60c';
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`);
       const { Search, totalResults } = res.data;
-      context.commit('updateState', {
+      commit('updateState', {
         movies : Search,
-        message : 'Hello World!',
-        loading : true,
       })
     }
   },
