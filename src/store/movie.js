60728f574a5bac2de7b0336794a06436e38eb3c1
completedLee <completed0728@gmail.com>
2021-10-30 19:46:43 +0900

    영화검색 추가기능 페이지 10, 20, 30로직 추가

--- src/store/movie.js
+++ src/store/movie.js
@@ -17,14 +17,33 @@ export default{
     }
   },
   actions: { 
-    async searchMovies({commit}, payload){
+    async searchMovies({ state, commit}, payload){
       const { title, type, number, year } = payload;
       const OMDB_API_KEY = '7035c60c';
+
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`);
       const { Search, totalResults } = res.data;
       commit('updateState', {
         movies : Search,
-      })
+      });
+      console.log(totalResults); // 278
+      console.log(typeof totalResults); // string
+      const total = parseInt(totalResults, 10);
+      const pageLength = Math.ceil(total / 10); // 28 올림처리
+
+      //추가 요청
+      if (pageLength > 1) {
+        for (let page = 2; page < pageLength; page++) {
+          if(page > number / 10){
+            break;
+          }
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`);
+          const { Search } = res.data;
+          commit('updateState',{
+            movies : [...state.movies, ...Search], //전개연산자
+          })
+        }
+      }
     }
   },
 }
